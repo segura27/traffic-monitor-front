8dcac1a59a456633ef3e7a658b51965afb40c950
Daniel Segura <segura2794@gmail.com>
2019-07-21 20:26:14 -0600

    Add device registration endpoint.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Devices from "./components/devices.jsx";
+import DeviceRegistration from "./components/devices_register";
 import TrafficData from "./components/traffic_data.jsx";
 import BadRequest from "./components/bad_request.jsx";
 import "bootstrap/dist/css/bootstrap.css";
@@ -12,6 +13,7 @@ function App() {
         <Switch>
           {/* Routes */}
           <Route path="/devices" exact component={Devices} />
+          <Route path="/devices/register" exact component={DeviceRegistration} />
           <Route path="/traffic_data" exact component={TrafficData} />
           <Route component={BadRequest} />
         </Switch>
